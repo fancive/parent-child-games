c9dd93d39c1d3e908e8059456b93dd752f209e74
wujunchen <wujunchen@baidu.com>
2026-04-26 16:01:36 +0800

    chore: exclude tests directory from ESLint (uses ESM imports)
    Change-Id: I686e0485b186a6c7541a737e51e5aa4bfa6c2ed7

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import prettier from 'eslint-config-prettier';
 
 export default [
   {
-    ignores: ['node_modules/**', 'godot-project/**', '.agent/**'],
+    ignores: ['node_modules/**', 'godot-project/**', '.agent/**', 'tests/**'],
   },
   {
     files: ['**/*.js'],
